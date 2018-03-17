c6db575745adfb1cca472c3ad6bc7e154e3c54d8
minhngoc2512 <minhngoc2512@yahoo.com>
2018-03-18 00:20:32 +0700

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,85 +1,79 @@
-$(document).ready(function(){
-    $(".tab-item").click(function(){
+$(document).ready(function () {
+    $(".tab-item").click(function () {
         var id_tab = $(this).attr('id');
-        if($(this).hasClass('active')){
+        if ($(this).hasClass('active')) {
             return false;
         }
         var id_tab_active = $('.tab-item.active').attr('id');
-        console.log('id_tab:'+id_tab);
-        console.log('actived:'+id_tab_active);
+        console.log('id_tab:' + id_tab);
+        console.log('actived:' + id_tab_active);
         $('.tab-item').removeClass('active');
         $(this).addClass('active');
-        $('#content-'+id_tab_active).removeClass('active-tab');
-        $('#content-'+id_tab).addClass('active-tab');
+        $('#content-' + id_tab_active).removeClass('active-tab');
+        $('#content-' + id_tab).addClass('active-tab');
     });
 
-    $('#btn-option').click(function(){
+    $('#btn-option').click(function () {
         var type_item = $(".type-item");
-        if(type_item.hasClass('type-1')){
+        if (type_item.hasClass('type-1')) {
             $(this).html('<img src="public/images/icons/type_2.png">');
-
             type_item.removeClass('type-1');
             type_item.addClass('type-2');
-            console.log('rm 1')
-        }else{
+        } else {
             $(this).html('<img src="public/images/icons/type_1.png">');
             type_item.removeClass('type-2');
             type_item.addClass('type-1');
-            console.log('rm 2');
         }
     });
-        var box1 = document.getElementsByClassName("touch-item");
-        var touchEvent;
-        var statusHold = false;
-        function touchHold(){
-            if(statusHold===false) {
-                clearEvent();
-                touchEvent = window.setTimeout(actionHold, 1000);
+    var box1 = document.getElementsByClassName("touch-item");
+    var touchEvent;
+    var statusHold = false;
+    var item_action;
+
+    function touchHold() {
+        if (statusHold === false) {
+            clearEvent();
+            touchEvent = window.setTimeout(actionHold, 500);
+        }
+    }
+
+    function actionHold() {
+        statusHold = true;
+        item_action.target.offsetParent.classList.add('active-content-touch');
+        clearEvent();
+    }
+
+    function clearEvent() {
+        if (touchEvent) {
+            clearTimeout(touchEvent);
+
+        }
+    }
+
+    for (var i = 0; i < box1.length; i++) {
+        var element = box1[i];
+        element.addEventListener('touchstart', function (e) {
+            var element = e.target.offsetParent.classList;
+            if(element.contains('active-content-touch')){
+                return;
+            }else{
+                var item_touch_old = $('.touch-item');
+                if(item_touch_old.hasClass('active-content-touch')){
+                    item_touch_old.removeClass('active-content-touch');
+                    statusHold = false;
+                }
+                item_action = e;
+                touchHold();
             }
-        }
-        function actionHold() {
-            statusHold=true;
-            console.log('status:'+statusHold);
-            box1.style.backgroundColor = "#CFCF00";
-        }
 
-        function clearEvent() {
-            if (touchEvent) {
-                console.log('clear event');
-                window.clearTimeout(touchEvent);
-            } //cancel timer when mouse button is released
-            box1.style.backgroundColor = "black";
-        }
-
-
-        box1.addEventListener('touchstart', function(e){
-            // alert('demo alert');
-            touchHold();
-            console.log('touchstart');
-            $('#text').html('<h1>sdfgdfg</h1>');
-        }, false);
-        //
-        box1.addEventListener('touchmove', function(e){
-
-            console.log(e);
 
         }, false);
-        //
-        box1.addEventListener('touchend', function(e){
-            if(statusHold===false) {
-                clearEvent();
-                // console.log(e);
-                console.log('touchend');
-            }
-        }, false);
-        var body = document.getElementById('body');
-        body.addEventListener('touchend', function(e){
-            if(e.target.className!=='active-event'){
-                clearEvent();
-                statusHold=false;
-
-                console.log('status:'+statusHold);
+        element.addEventListener('touchend', function (e) {
+            clearEvent();
+            var element = e.target.offsetParent.classList;
+            if(element.contains('active-content-touch')){
             }
         }, false);
+    }
 });
 
